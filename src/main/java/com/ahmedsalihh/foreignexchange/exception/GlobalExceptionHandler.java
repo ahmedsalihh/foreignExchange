bf22e3d4663a93077147822e1a9031188c17dea5
ahmedsalihh <ahmedsalihh@gmail.com>
2020-07-21 02:36:06 +0300

    Add log4j2 logger

--- src/main/java/com/ahmedsalihh/foreignexchange/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ahmedsalihh/foreignexchange/exception/GlobalExceptionHandler.java
@@ -2,8 +2,8 @@ package com.ahmedsalihh.foreignexchange.exception;
 
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,40 +12,40 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), 1000, request.getDescription(false));
-        logger.error("{}", errorDetails.getMessage());
+        LOGGER.error("{}", errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NoParameterProvidedException.class)
     public ResponseEntity<?> noParameterProvidedException(NoParameterProvidedException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), 1001, request.getDescription(false));
-        logger.error("{}", errorDetails.getMessage());
+        LOGGER.error("{}", errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DateNotRecognizedException.class)
     public ResponseEntity<?> dateNotRecognizedException(DateNotRecognizedException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), 1002, request.getDescription(false));
-        logger.error("{}", errorDetails.getMessage());
+        LOGGER.error("{}", errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidCurrencyException.class)
     public ResponseEntity<?> invalidCurrencyException(InvalidCurrencyException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), 1003, request.getDescription(false));
-        logger.error("{}", errorDetails.getMessage());
+        LOGGER.error("{}", errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), 500, request.getDescription(false));
-        logger.error("{}", errorDetails.getMessage());
+        LOGGER.error("{}", errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
